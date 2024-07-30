e8dfe1e671cf71d41a65a0f9f41502eb5ccdcdbd
ankuragicha <ankuragicha@gmail.com>
2024-07-30 16:52:43 +0530

    updated changes

--- pdfkg.py
+++ pdfkg.py
@@ -17,6 +17,9 @@ os.environ["NEO4J_PASSWORD"]=os.getenv("NEO4J_PASSWORD")
 os.environ["Groq_api_key"] = os.getenv("GROQ_API_KEY")
 llm=ChatGroq(groq_api_key=Groq_api_key,model_name="Gemma-9b-it")
 
+from neo4j import GraphDatabase
+driver = GraphDatabase.driver(uri=NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
+
 def connect_to_neo():
     graph=Neo4jGraph(
     url=NEO4J_URI,
@@ -36,6 +39,27 @@ def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
     chunks = text_splitter.split_text(text)
     return chunks
+
+def create_node(tx, node_id, node_type):
+    query = f"CREATE (n:{node_type} {{ id: $id }})"
+    tx.run(query, id=node_id)
+
+def create_relationship(tx, source_id, relationship_type, target_id):
+    query = (
+        f"MATCH (a {{id: $source_id}}), (b {{id: $target_id}}) "
+        f"CREATE (a)-[r:{relationship_type}]->(b) "
+        "RETURN type(r)"
+    )
+    tx.run(query, source_id=source_id, target_id=target_id)
+
+def load_graph_documents_to_neo4j(graph_documents):
+    with driver.session() as session:
+        for node in graph_documents[0].nodes:
+            session.execute_write(create_node, node.id, node.type)
+ 
+        for relationship in graph_documents[0].relationships:
+            session.execute_write(create_relationship, relationship.source.id, relationship.type, relationship.target.id)
+
 
 
 def main():
@@ -58,7 +82,8 @@ def main():
                 documents=[Document(page_content=text_chunks)]
                 llm_transformer=LLMGraphTransformer(llm=llm)
                 graph_documents=llm_transformer.convert_to_graph_documents(documents)
-                #get_vector_store(text_chunks)
+                load_graph_documents_to_neo4j(graph_documents)
+                driver.close()
                 st.success("Done")
 
 if __name__ == "__main__":
